af33bbeddf82fac5b18b663bcbfd955f55797c40
Ruben Bueno <61913555+BuenoRuben@users.noreply.github.com>
2026-03-06 18:55:44 +0100

    Switch model from RandomForest to IsolationForest
    Changed the model from RandomForestClassifier to IsolationForest for label compatibility.

--- solution/submission.py
+++ solution/submission.py
@@ -1,7 +1,7 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import IsolationForest
 
 
 # The submission here should simply be a function that returns a model
 # compatible with scikit-learn API
 def get_model():
-    return RandomForestClassifier()
+    return IsolationForest() # will give labels like -1 or 1, not 0 or 1, so might be bad depebnding on what was done for the labelingh part of the data
